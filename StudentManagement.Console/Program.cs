66e9bfa46e35bb6adf67a6a270a743d07e1a1686
macie <melastor99@gmail.com>
2023-03-16 13:43:03 +0100

    First commit for student management project

---
+++ StudentManagement.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_StudentClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student());
+            Console.WriteLine("Enter student name");
+            students[0].name = Console.ReadLine();
+            Console.WriteLine("Enter student subname");
+            students[0].subname = Console.ReadLine();
+            Console.WriteLine("What the student is studying");
+            students[0].occupation = Console.ReadLine();
+            Console.WriteLine("Enter student id");
+            students[0].id = int.Parse(Console.ReadLine());
+            Console.WriteLine("How old is student");
+            students[0].age = int.Parse(Console.ReadLine());
+            students[0].GetFullInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentManagement.Console/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_StudentClass
+{
+    class Student
+    {
+        public string name { get; set; }
+        public string subname { get; set; }
+        public string occupation { get; set; }
+        public int age { get; set; }
+        public double id { get; set; }
+
+        //public Student(string studentName, string studentSubname,string studentOccupation, int studentAge, double studentId)
+        //{
+        //    name = studentName;
+        //    subname = studentSubname;
+        //    occupation = studentOccupation;
+        //    age = studentAge;
+        //    id = studentId;
+
+        //}
+        public void GetFullInfo()
+        {
+            Console.WriteLine("Student name is {0} {1}", name, subname);
+            Console.WriteLine("He is studying {0}", occupation);
+            Console.WriteLine("He's id is {0}", id);
+            Console.WriteLine("He is {0} years old", age);
+        }
+
+    }
+
+}
