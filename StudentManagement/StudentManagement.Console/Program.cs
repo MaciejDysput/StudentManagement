1596ff7f9e040a2d04c3866a6db4fce843a038a3
macie <melastor99@gmail.com>
2023-03-16 14:34:53 +0100

    Initial commit

---
+++ StudentManagement/StudentManagement.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using StudentManagement.Console;
+
+List<Student> students = new List<Student>();
+students.Add(new Student());
+Console.WriteLine("Enter student name");
+students[0].Name = Console.ReadLine();
+Console.WriteLine("Enter student subname");
+students[0].Surname = Console.ReadLine();
+Console.WriteLine("What the student is studying");
+students[0].Occupation = Console.ReadLine();
+Console.WriteLine("Enter student id"); students[0].Id = int.Parse(Console.ReadLine());
+Console.WriteLine("How old is student");
+students[0].Age = int.Parse(Console.ReadLine());
+students[0].GetFullInfo();
+Console.ReadLine();
---
+++ StudentManagement/StudentManagement.Console/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Console
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Occupation { get; set; }
+        public int Age { get; set; }
+        public double Id { get; set; }
+
+
+        public string GetFullInfo()
+        {
+            return $"Student name is {Name} {Surname}." +
+                $"He studies {Occupation}" +
+                $" His id is {Id}" +
+                $"And he is {Age} years old";
+        }
+
+    }
+
+}
