57be46eb9f3e2a0c200c9a50419143468c50f2f9
macie <melastor99@gmail.com>
2023-03-20 11:02:13 +0100

    deleted switch and case, kept a ability to add a person

--- StudentManagement/StudentManagement.Console/Program.cs
+++ StudentManagement/StudentManagement.Console/Program.cs
@@ -1,90 +1,46 @@
-﻿using Newtonsoft.Json;
-using StudentManagement.Console;
-using System.Text;
+﻿using StudentManagement.Console;
 
-bool menu = true;
 var students = new List<Student>();
 string name, surname, occupation;
 int age;
 
-do
+Console.WriteLine("Enter student name");
+name = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(name))
 {
+    Console.WriteLine("Name cannot be empty. Please enter your name:");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine("Enter student surname");
+surname = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(surname))
+{
+    Console.WriteLine("Surname cannot be empty. Please enter your surname:");
+    surname = Console.ReadLine();
+}
+Console.WriteLine("What the student is studying");
+occupation = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(occupation))
+{
+    Console.WriteLine("Surname cannot be empty. Please enter your occupation:");
+    occupation = Console.ReadLine();
+}
+Console.WriteLine("How old is student");
+while (!int.TryParse(Console.ReadLine(), out age))
+{
+    Console.WriteLine("Incorrect value, Please enter your age:");
+}
+int Age = age;
+
+var student = new Student() { Name = name, Surname = surname, Occupation = occupation, Age = Age };
+Console.WriteLine($"Student id is {student.Id}");
+Console.WriteLine("Succesfully added a student. {0} {1} studies {2}. His id is {3} and he is {4}\n", student.Name, student.Surname, student.Occupation, student.Id, student.Age);
+students.Add(student);
 
 
-    Console.WriteLine("Press 1 to insert student");
-    Console.WriteLine("Press 2 to display student info");
-    Console.WriteLine("\nChoose Option");
-    int option = int.Parse(Console.ReadLine());
 
 
 
 
-    switch (option)
-    {
-        case 1:
-            Console.WriteLine("Enter student name");
-            name = Console.ReadLine();
-            while (string.IsNullOrWhiteSpace(name))
-            {
-                Console.WriteLine("Name cannot be empty. Please enter your name:");
-                name = Console.ReadLine();
-            }
-
-            Console.WriteLine("Enter student surname");
-            surname = Console.ReadLine();
-            while (string.IsNullOrWhiteSpace(surname))
-            {
-                Console.WriteLine("Surname cannot be empty. Please enter your surname:");
-                surname = Console.ReadLine();
-            }
-            Console.WriteLine("What the student is studying");
-            occupation = Console.ReadLine();
-            while (string.IsNullOrWhiteSpace(occupation))
-            {
-                Console.WriteLine("Surname cannot be empty. Please enter your occupation:");
-                occupation = Console.ReadLine();
-            }
-            Console.WriteLine("How old is student");
-            while (!int.TryParse(Console.ReadLine(), out age))
-            {
-                Console.WriteLine("Incorrect value, Please enter your age:");
-            }
-            int Age = age;
-
-
-            var student = new Student() { Name = name, Surname = surname, Occupation = occupation, Age = Age };
-            Console.WriteLine($"Student id is {student.Id}");
-            Console.WriteLine("Succesfully added a student. {0} {1} studies {2}. His id is {3} and he is {4}\n", student.Name, student.Surname, student.Occupation, student.Id, student.Age);
-            students.Add(student);
-            
-            
-            
-            break;
-        case 2:
-
-            Student[] arrayOfStudents = students.ToArray();
-            for (int i = 0; i < arrayOfStudents.Length; i++)
-            {
-                Console.WriteLine(arrayOfStudents[i].Name + " " + arrayOfStudents[i].Surname + " " + "studies" + " " + arrayOfStudents[i].Occupation + " " + "He is " + arrayOfStudents[i].Age + " " + "years old. His id is" + " " + arrayOfStudents[i].Id);
-            }
-            //foreach (Student studentList in students)
-            //{
-            //    Console.WriteLine(studentList);
-            //}
-            
-            break;
-        default: break;
-    }
-    Console.WriteLine("Back to Menu? y/n");
-    char c = char.Parse(Console.ReadLine());
-
-    if (c == 'y')
-    {
-        menu = true;
-    }
-    else
-    {
-        menu = false;
-    }
-} while (menu);
 
--- StudentManagement/StudentManagement.Console/Student.cs
+++ StudentManagement/StudentManagement.Console/Student.cs
@@ -1,33 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentManagement.Console
+﻿namespace StudentManagement.Console
 {
-    [Serializable]
     public class Student
     {
-        
+
         public string Name { get; init; }
         public string Surname { get; init; }
         public string Occupation { get; init; }
         public int Age { get; init; }
-        public double Id { get; init; } = Math.Round(Random.Shared.NextDouble() * 10);
-
-        
-
-        //public string GetFullInfo()
-        //{
-
-        //    return $"Student name is {Name} {Surname}." +
-        //        $"He studies {Occupation}" +
-        //        $" His id is {Id}" +
-        //        $"And he is {Age} years old";
-        //}
-
-
+        public int Id { get; init; } = Random.Shared.Next(1, 1000);
     }
 
 }
